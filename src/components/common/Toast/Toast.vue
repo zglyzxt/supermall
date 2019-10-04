4dbf8c4dabeb6fba7c059ded76695b52d6d63e58
xtzhang <xtzhang1995@hotmail.com>
2019-10-04 22:42:08 +0800

    postcss

---
+++ src/components/common/Toast/Toast.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="toast">
+    <div>{{}}</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Toast",
+    props: {
+     message: {
+       type:String,
+       default:''
+     }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,17 @@ import Vue from 'vue'
 import App from './App.vue'
 import  router from './router'
 
-Vue.config.productionTip = false
-Vue.prototype.$bus = new Vue()
+import FastClick from 'fastclick'
+import VueLazyLoad from 'vue-lazyload'
 
+Vue.config.productionTip = false
+//添加时间总线
+Vue.prototype.$bus = new Vue()
+//解决的移动端300毫秒延迟
+FastClick.attach(document.body)
+Vue.use(VueLazyLoad,{
+  loading: require('./assets/img/common/placeholder.png')
+})
 
 new Vue({
   render: h => h(App),
